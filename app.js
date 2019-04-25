0c2bfafb98e3b1a2a69d4377b67098ac83b0e5ea
julienb86 <julien.bouafia@gmail.com>
2019-04-25 21:06:50 +0200

    added connection to MongoDB

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://julien:7KyvgNH20VoZqf2G@cluster0-psxly.mongodb.net/test?retryWrites=true')
+    .then(
+        () => {
+            console.log("Successfully connected to MongoDB Atlas!");         
+        }
+    ).catch(
+        (error) => {
+            console.log("Unable to connect to MongoDB Atlas!");
+            console.error(error);
+        }
+    );
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end("this my server response");
-});
+const app = require('./app')
+const server = http.createServer(app);
+
+app.set('PORT', process.env.PORT || 3000);
 
 server.listen(process.env.PORT || 3000);
